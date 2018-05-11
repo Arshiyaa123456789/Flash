19358764bcf629e9b59eedd614847b537d766385
pc33 <pc33@PC33.IDMSYSTECH.id>
2018-05-11 12:06:53 +0530

    first commit
    Uploading all the files

---
+++ DemoTest/src/Demo/DemoA.java
@@ -0,0 +1,11 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoA {
+@Test
+public void printA()
+{
+	System.out.println("A..");
+}
+}
---
+++ DemoTest/src/Demo/DemoB.java
@@ -0,0 +1,11 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoB {
+@Test
+	public void printB()
+	{
+		System.out.println("B..");
+	}
+}
